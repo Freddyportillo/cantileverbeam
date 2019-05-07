562346fe286d0faf1c99e35f2f6bcdb56edd50cb
Alejandro Portillo <freddyalejandropm@ufps.edu.co>
2019-05-07 10:00:40 -0300

    foram calculados os deslocamentos para os gdl livres

--- cantilever_ex.f95
+++ cantilever_ex.f95
@@ -2,14 +2,14 @@ program cantilever_ex
 
     implicit none
     
-        !
-        real, dimension (3,3) :: prop, kll
+        !Foi gerada a submatriz de rigidez kll
+        real*8, dimension (3,3) :: prop, kll, invkll
         real, dimension (3,1) ::  kli, kii
         real, dimension (1,2) :: cc
         real, dimension (3,2) :: fa
         integer, dimension (3,2) :: mat_conect
-        integer :: ii, nele, nnodes, ngdl
-        real, dimension (3) :: coef
+        integer :: ii, nele, nnodes, ngdl,n
+        real, dimension (3) :: coef, u_liv
         integer, dimension (3) :: gdl_livres
         real, dimension (2,2) :: kop, kele
         integer, dimension (4,4) :: ident
@@ -17,7 +17,7 @@ program cantilever_ex
         real, dimension (4,2) :: transmat
         real, dimension (4,4) :: kglobal, kglobal1
         real :: a,e
-        integer,dimension (1,1) :: gdl_impostos
+        integer,dimension (1) :: gdl_impostos
             nele = 3
             nnodes = 4
             ngdl = 4
@@ -67,8 +67,19 @@ program cantilever_ex
             
             kll = kglobal(gdl_livres,gdl_livres)
             kli = kglobal(gdl_livres,gdl_impostos)
-            kii = kglobal(gdl_impostos,gdl_impostos)
-
+            print*, '----------'
+            print*, kll
+            print*, '----------'
+            print*, kli
+            !kii = kglobal(gdl_impostos,gdl_impostos)
+            
+            !deslocamentos no gdl gdl_livres
+            n = 3.0
+            call inv(kll, invkll, n)
+            
+            u_liv = matmul(invkll,fa(:,2))*1000
+            print*, '----------'
+            print*,u_liv
 end program
                     
         subroutine matmu (A,B,C,D)
@@ -83,5 +94,67 @@ end program
   D = matmul (mul,C)
   
     end subroutine matmu
+    
+    
+    
+    subroutine inv(a,c,n)
+!============================================================
+! Inverse matrix
+! Method: Based LU decomposition for Ax=b
+! Marouf Abderahmane - Strasbourg University 2016 
+!-----------------------------------------------------------
+implicit none 
+integer n
+double precision a(n,n), c(n,n)
+double precision L(n,n), U(n,n), b(n), d(n), x(n)
+double precision coeff
+integer i, j, k
+
+L=0.0
+U=0.0
+b=0.0
+
+do k=1, n-1
+   do i=k+1,n
+      coeff=a(i,k)/a(k,k)
+      L(i,k) = coeff
+      do j=k+1,n
+         a(i,j) = a(i,j)-coeff*a(k,j)
+      end do
+   end do
+end do
+
+do i=1,n
+  L(i,i) = 1.0
+end do
+do j=1,n
+  do i=1,j
+    U(i,j) = a(i,j)
+  end do
+end do
+
+do k=1,n
+  b(k)=1.0
+  d(1) = b(1)
+  do i=2,n
+    d(i)=b(i)
+    do j=1,i-1
+      d(i) = d(i) - L(i,j)*d(j)
+    end do
+  end do
+  x(n)=d(n)/U(n,n)
+  do i = n-1,1,-1
+    x(i) = d(i)
+    do j=n,i+1,-1
+      x(i)=x(i)-U(i,j)*x(j)
+    end do
+    x(i) = x(i)/u(i,i)
+  end do
+  do i=1,n
+    c(i,k) = x(i)
+  end do
+  b(k)=0.0
+end do
+end subroutine inv
              
                 
